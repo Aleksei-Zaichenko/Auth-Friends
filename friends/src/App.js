c6d99f9a32a27acb768ea09cc6348e958692d9f5
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-03-25 00:26:26 -0400

    created a login page

---
+++ friends/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Login} from './components/LoginPage';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
